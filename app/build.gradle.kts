41c577ca43a9da4faa633c3b3705d70977dc5b88
Tangobear <tangobearwiki@gmail.com>
2026-09-06 00:21:20 +0800

    配置 Android 16 自动编译流程

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.svewiki.editor"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.svewiki.editor"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0.0"
     }
